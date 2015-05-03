3beb73d449636498d2d035b1b98c556b047ff69d
flramirezs <flramirezs@unal.edu.co>
2015-05-03 09:46:26 -0500

    -Aadición de gui-3 (falta comportamiento de botón "borrar personaje")

---
+++ UNReal_Fantasy/Assets/UIElements/Scripts/buttonEvent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonEvent : MonoBehaviour {
+
+	public Renderer rend;
+	public Texture2D inactiveTexture;
+	public Texture2D pressedTexture;
+	public Texture2D activeTexture;
+	public string sceneName;
+	
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	void OnMouseEnter() {
+		rend.material.mainTexture=activeTexture;
+	}
+	void OnMouseDown() {
+		rend.material.mainTexture=pressedTexture;
+		Application.LoadLevel(sceneName);
+	}
+	void OnMouseUp(){
+		rend.material.mainTexture=activeTexture;
+	}
+	void OnMouseExit() {
+		rend.material.mainTexture=inactiveTexture;
+	}
+
+}
---
+++ UNReal_Fantasy/Assets/UIElements/Scripts/rotatePJ.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotatePJ : MonoBehaviour {
+	
+	public int speed=10;
+	public float friction=5;
+	public float lerpSpeed=5;
+	private float xDeg;
+	private float yDeg;
+	private Quaternion fromRotation;
+	private Quaternion toRotation ;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)) {
+			xDeg -= Input.GetAxis("Mouse X") * speed * friction;
+			yDeg += Input.GetAxis("Mouse Y") * speed * friction;
+			fromRotation = transform.rotation;
+			toRotation = Quaternion.Euler(0,xDeg,0);
+			transform.rotation = Quaternion.Lerp(fromRotation,toRotation,Time.deltaTime  * lerpSpeed);
+		}
+	}
+
+	void onMouseEnter(){
+		Debug.Log("hola mouseenter");
+	}
+}
