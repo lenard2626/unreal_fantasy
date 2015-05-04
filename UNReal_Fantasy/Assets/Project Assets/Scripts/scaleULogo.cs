98cb849ce0adf1875ad3c963a2215bc7fec849fb
davidsneos <dcdelgadoa@unal.edu.co>
2015-05-04 02:29:37 -0500

    -Adicion de escenas y modificacion de assets
    -Añadidas escenas de gui-1 y gui-4
    -Modificado el formato de los sonidos y las voces

---
+++ UNReal_Fantasy/Assets/Project Assets/Scripts/scaleULogo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scaleULogo : MonoBehaviour {
+	public float maxScale=0.4f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.localScale.x < maxScale) {
+			Vector3 tempScale = transform.localScale;
+			transform.localScale = tempScale + new Vector3 (0.01f, 0.01f, 0.01f);
+		}
+	}
+}
