f1f07264f20fc4b2dde2741b342ab1ccb9b34b56
flramirezs <flramirezs@unal.edu.co>
2015-05-04 21:46:12 -0500

    -commit de escenas

---
+++ UNReal_Fantasy/Assets/Escena de escudo/Scripts/loadNextScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadNextScene : MonoBehaviour {
+
+	public int timeout=2;
+
+	private int timestart=0;
+	private int timefinish=100;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timefinish < timestart) {
+			Application.LoadLevel("gui-5");
+		} else {
+			timestart+=1;
+		}
+	}
+}
---
+++ UNReal_Fantasy/Assets/Escena de felicitacion/Scripts/shieldBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class shieldBehaviour : MonoBehaviour {
+
+	public int speedY=10;
+	public int maxDeltaY=100;
+	public int timeoutAfterCongrats=2;
+
+	private int countY=0;
+	private Text congratsTextRef=null;
+	private int timeoutACCount=0;
+
+	// Use this for initialization
+	void Start () {
+		congratsTextRef= GetTextObject();
+		congratsTextRef.material.color=new Color(0,0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (countY < maxDeltaY) {
+			transform.Translate (Vector3.up * Time.deltaTime * speedY);
+			countY += speedY;
+		} else {
+			congratsTextRef.material.color=new Color(255,255,255,1.0f);
+			if(timeoutACCount<timeoutAfterCongrats){
+				timeoutACCount+=1;
+			}else{
+				Application.LoadLevel("MainWorld");
+			}
+		}
+	}
+	private Text GetTextObject()
+	{
+		GameObject canvas = GameObject.Find("Canvas");
+		Text[] textValue = canvas.GetComponentsInChildren<Text>();
+		return textValue[0];
+	}
+}
