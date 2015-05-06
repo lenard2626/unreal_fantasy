aef6d97941a46255767076102f24f5884b5f8a4f
jdtorregrosas <jdtorregrosas@unal.edu.co>
2015-05-06 16:29:18 -0500

    MenuPausa rudimentario implementado

---
+++ UNReal_Fantasy/Assets/MenuPausa/EscapeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeScript : MonoBehaviour {
+
+	public PauseMenu scriptMenu;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("escape")) {
+			Time.timeScale = 1;
+			//show the pause menu
+			scriptMenu = GetComponent<PauseMenu>();
+			scriptMenu.enabled = true;
+		}
+	}
+}
