d2a816ade1bbb1a8f29e81dd695a4f30bb11f3ff
Camilo Delgado (Neos) <dcdelgadoa@unal.edu.co>
2015-05-07 23:14:24 -0500

    -Modificado audio de intro y añadido audio de naturaleza para MainWorld.unity

--- UNReal_Fantasy/Assets/Project Assets/Scripts/destroyAudio.cs
+++ UNReal_Fantasy/Assets/Project Assets/Scripts/destroyAudio.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class destroyAudio : MonoBehaviour {
-
+	public GameObject introAudio;
 	// Use this for initialization
 	void Start () {
-		Destroy (dontDestroyOnLoad.instance, 0.5f);
+		//Destroy the introAudio
+		introAudio = GameObject.Find("IntroAudio");
+		Destroy (introAudio);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
