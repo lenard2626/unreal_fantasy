946e69e2462519402335163c27db68bfb7a5147f
davidsneos <davidcamiloneo@gmail.com>
2015-05-12 08:50:35 -0500

    -Fix del menu de pausa

--- UNReal_Fantasy/Assets/MenuPausa/EscapeScript.cs
+++ UNReal_Fantasy/Assets/MenuPausa/EscapeScript.cs
@@ -4,14 +4,22 @@ using System.Collections;
 public class EscapeScript : MonoBehaviour {
 
 	public PauseMenu scriptMenu;
-	
+	private bool menuIsClosed = true;
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey("escape")) {
+		if(Input.GetKeyUp("escape")&&menuIsClosed) {
 			Time.timeScale = 1;
 			//show the pause menu
 			scriptMenu = GetComponent<PauseMenu>();
 			scriptMenu.enabled = true;
+			menuIsClosed = false;
+		}else if(Input.GetKeyUp("escape")&&!menuIsClosed){
+			Time.timeScale = 1;
+			//show the pause menu
+			scriptMenu = GetComponent<PauseMenu>();
+			scriptMenu.enabled = false;
+			menuIsClosed = true;
+
 		}
 	}
 }
