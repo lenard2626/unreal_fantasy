89c6bdda7afbf49d0b8713819e2b2b469a0e08df
jdtorregrosas <jdtorregrosas@unal.edu.co>
2015-05-14 18:11:01 -0500

    Tiggers de saludo
    Triggers de saludos implementados
    Jerarquía de carpetas mejorada

---
+++ UNReal_Fantasy/Assets/Project Assets/Scripts/TriggerSaludo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerSaludo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnTriggerEnter() { 
+		GetComponent<AudioSource>().Play(); 
+	} 
+
+	void OnTriggerExit() {
+		GetComponent<AudioSource>().Stop(); 
+	}
+}
---
+++ UNReal_Fantasy/Assets/Project Assets/Scripts/rotateEtwas.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateEtwas : MonoBehaviour {
+
+	public int speedY=10;
+	public int maxDeltaY=100;
+	private int countY=0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (countY < maxDeltaY) {
+			transform.Translate (Vector3.up * Time.deltaTime * speedY);
+			countY += speedY;
+		}
+	}
+}
