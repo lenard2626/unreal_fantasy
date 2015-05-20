92567eb4f993ea6ee61829b12bc15860b59a2069
jdtorregrosas <jdtorregrosas@unal.edu.co>
2015-05-19 22:10:01 -0500

    Leon acomodado Dialogos con click habilitables dentro del trigger boton cancelar funcional

--- UNReal_Fantasy/Assets/Project Assets/Scripts/mostrardialogo.cs
+++ UNReal_Fantasy/Assets/Project Assets/Scripts/mostrardialogo.cs
@@ -13,6 +13,7 @@ public class mostrardialogo : MonoBehaviour {
 	mostrardialogo scriptDialog;
 	public Font fuente;
 	bool isOpened=false;
+	bool isOnTrigger=false;
 
 	// Use this for initialization
 	void Start () {
@@ -44,16 +45,22 @@ public class mostrardialogo : MonoBehaviour {
 		GUI.skin.textArea.focused.background = null;
 		GUI.skin.textArea.normal.textColor = Color.white;
 		GUI.skin.font = fuente;
-		Rect rectText = new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7);
+
+		Rect rectText = new Rect (Screen.width / 6, Screen.width / 100, Screen.width / 6, Screen.height / 7);
+		Rect rectBtn1 = new Rect (Screen.width * 4 / 6, Screen.width / 80, Screen.width / 7, Screen.height / 11);
+		Rect rectBtn2 = new Rect (Screen.width * 5 / 6, Screen.width / 80, Screen.width / 7, Screen.height / 11);
+
+
 		if (this.transform.parent.name == "Jimmy") {
 			if(player.state==0){
 				GUI.TextArea (rectText, "Haga la mision 1");
-				if(GUI.Button(new Rect(Screen.width*4/6, Screen.width/80, Screen.width/6, Screen.height/10), "Aceptar")) {
+				if(GUI.Button(rectBtn1, "Aceptar")) {
 					player.addMission("Mision 1");
 					player.listMissions();
 					GUI.TextArea (rectText, "Mision 1 Aceptada");
-				}else if (GUI.Button(new Rect(Screen.width*5/6, Screen.width/80, Screen.width/6, Screen.height/10), "Cancelar")){
+				}else if (GUI.Button(rectBtn2, "Cancelar")){
 					GUI.TextArea (rectText, "Vuelve Pronto");
+					isOpened=false;
 				}
 			}else{
 				GUI.TextArea (rectText, "Hola, como estas, en que puedo ayudarte!");
@@ -62,12 +69,13 @@ public class mostrardialogo : MonoBehaviour {
 		} else if(this.transform.parent.name == "Nigga"){
 			if(player.state==1){
 				GUI.TextArea (rectText, "Haga la mision 2");
-				if(GUI.Button(new Rect(Screen.width*4/6, Screen.width/80, Screen.width/6, Screen.height/10), "Aceptar")) {
+				if(GUI.Button(rectBtn1, "Aceptar")) {
 					player.addMission("Mision 2");
 					player.listMissions();
 					GUI.TextArea (rectText, "Mision 2 Aceptada");
-				}else if (GUI.Button(new Rect(Screen.width*5/6, Screen.width/80, Screen.width/6, Screen.height/10), "Cancelar")){
+				}else if (GUI.Button(rectBtn2, "Cancelar")){
 					GUI.TextArea (rectText, "Vuelva Pronto, oyo?");
+					isOpened=false;
 				}
 			}else{
 				GUI.TextArea (rectText, "Que necesita el 'eñor!!");
@@ -76,12 +84,13 @@ public class mostrardialogo : MonoBehaviour {
 		} else if(this.transform.parent.name == "ProfesorCuchilla"){
 			if(player.state==2){
 				GUI.TextArea (rectText, "Haga la mision 3");
-				if(GUI.Button(new Rect(Screen.width*4/6, Screen.width/80, Screen.width/6, Screen.height/10), "Aceptar")) {
+				if(GUI.Button(rectBtn1, "Aceptar")) {
 					player.addMission("Mision 3");
 					player.listMissions();
 					GUI.TextArea (rectText, "Mision 3 Aceptada");
-				}else if (GUI.Button(new Rect(Screen.width*5/6, Screen.width/80, Screen.width/6, Screen.height/10), "Cancelar")){
+				}else if (GUI.Button(rectBtn2, "Cancelar")){
 					GUI.TextArea (rectText, "Apurele!!");
+					isOpened=false;
 				}
 			}else{
 				GUI.TextArea (rectText, "Estudie Vago!!!");
@@ -90,12 +99,13 @@ public class mostrardialogo : MonoBehaviour {
 		} else if(this.transform.parent.name == "Celador"){
 			if(player.state==3){
 				GUI.TextArea (rectText, "Haga la mision 4");
-				if(GUI.Button(new Rect(Screen.width*4/6, Screen.width/80, Screen.width/6, Screen.height/10), "Aceptar")) {
+				if(GUI.Button(rectBtn1, "Aceptar")) {
 					player.addMission("Mision 4");
 					player.listMissions();
 					GUI.TextArea (rectText, "Mision 4 Aceptada");
-				}else if (GUI.Button(new Rect(Screen.width*5/6, Screen.width/80, Screen.width/6, Screen.height/10), "Cancelar")){
+				}else if (GUI.Button(rectBtn2, "Cancelar")){
 					GUI.TextArea (rectText, "Lo espero dotor");
+					isOpened=false;
 				}
 			}else{
 				GUI.TextArea (rectText, "El carnet por favor");
@@ -104,12 +114,13 @@ public class mostrardialogo : MonoBehaviour {
 		} else if(this.transform.parent.name == "Capucho"){
 			if(player.state==4){
 				GUI.TextArea (rectText, "Haga la mision 5");
-				if(GUI.Button(new Rect(Screen.width*4/6, Screen.width/80, Screen.width/6, Screen.height/10), "Aceptar")) {
+				if(GUI.Button(rectBtn1, "Aceptar")) {
 					player.addMission("Mision 5");
 					player.listMissions();
 					GUI.TextArea (rectText, "Mision 5 Aceptada");
-				}else if (GUI.Button(new Rect(Screen.width*5/6, Screen.width/80, Screen.width/6, Screen.height/10), "Cancelar")){
+				}else if (GUI.Button(rectBtn2, "Cancelar")){
 					GUI.TextArea (rectText, "Lo espero compita");
+					isOpened=false;
 				}
 			}else{
 				GUI.TextArea (rectText, "Camilo VIVE!!!");
@@ -131,68 +142,17 @@ public class mostrardialogo : MonoBehaviour {
 
 	void OnTriggerEnter(){
 		isOpened = true;
-		/*dialogo.enabled=true;
-		if (this.transform.parent.name == "Jimmy") {
-			imagen.sprite = this.sprite;
-			if(player.state==0){
-				player.addMission("Mision 1");
-				texto.text = "Haga la mision 1";
-				player.listMissions();
-			}else{
-				texto.text = "Hola, como estas, en que puedo ayudarte";
-			}
-			//texto.text = this.transform.parent.name + (this.transform.parent.name == "Jimmy");
-		} else if(this.transform.parent.name == "Nigga"){
-			imagen.sprite = this.sprite;
-			if(player.state==1){
-				player.completeMission("Mision 1");
-				player.addMission("Mision 2");
-				texto.text = "Haga la mision 2";
-				player.listMissions();
-			}else{
-				texto.text = "Que anda buscando el 'eñor";
-			}
-			//texto.text = this.transform.parent.name + (this.transform.parent.name == "Capucho");
-		} else if(this.transform.parent.name == "ProfesorCuchilla"){
-			imagen.sprite = this.sprite;
-			if(player.state==2){
-				player.completeMission("Mision 2");
-				player.addMission("Mision 3");
-				texto.text = "Haga la mision 3";
-				player.listMissions();
-			}else{
-				texto.text = "Vaya a estudiar vago!!";
-			}
-			
-			//texto.text = this.transform.parent.name + (this.transform.parent.name == "Capucho");
-		} else if(this.transform.parent.name == "Celador"){
-			imagen.sprite = this.sprite;
-			if(player.state==3){
-				player.completeMission("Mision 3");
-				player.addMission("Mision 4");
-				texto.text = "Haga la mision 4";
-				player.listMissions();
-			}else{
-				texto.text = "El carnet por favor!";
-			}
-			//texto.text = this.transform.parent.name + (this.transform.parent.name == "Capucho");
-		} else if(this.transform.parent.name == "Capucho"){
-			imagen.sprite = this.sprite;
-			if(player.state==4){
-				player.completeMission("Mision 4");
-				player.addMission("Mision 5");
-				texto.text = "Haga la mision 5";
-				player.listMissions();
-			}else{
-				texto.text = "CAMILO VIVE!!!";
-			}
-			//texto.text = this.transform.parent.name + (this.transform.parent.name == "Capucho");
-		}*/
-
+		isOnTrigger = true;
 	}
 
 	void OnTriggerExit(){
 		isOpened = false;
-		//dialogo.enabled=false;
+		isOnTrigger = false;
+	}
+
+	void OnMouseUp(){
+		if (isOnTrigger) {
+			isOpened = true;
+		}
 	}
 }
