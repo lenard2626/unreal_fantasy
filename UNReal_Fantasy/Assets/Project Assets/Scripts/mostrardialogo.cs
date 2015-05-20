d461ca34006062aaf35f9b9e647becb16a76305d
jdtorregrosas <jdtorregrosas@unal.edu.co>
2015-05-19 20:58:08 -0500

    dialogo con botones en general

--- UNReal_Fantasy/Assets/Project Assets/Scripts/mostrardialogo.cs
+++ UNReal_Fantasy/Assets/Project Assets/Scripts/mostrardialogo.cs
@@ -9,10 +9,11 @@ public class mostrardialogo : MonoBehaviour {
 	private Button aceptarMision;
 	private Button cancelarMision;
 	public Texture2D sprite;//Sprite
-	public registroJugador player;
-	public mostrardialogo scriptDialog;
+	registroJugador player;
+	mostrardialogo scriptDialog;
 	public Font fuente;
 	bool isOpened=false;
+
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find("PersonajePrincipal").GetComponent<registroJugador>();
@@ -43,60 +44,75 @@ public class mostrardialogo : MonoBehaviour {
 		GUI.skin.textArea.focused.background = null;
 		GUI.skin.textArea.normal.textColor = Color.white;
 		GUI.skin.font = fuente;
-
+		Rect rectText = new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7);
 		if (this.transform.parent.name == "Jimmy") {
 			if(player.state==0){
-				GUI.TextArea (new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7), "Haga la mision 1");
+				GUI.TextArea (rectText, "Haga la mision 1");
 				if(GUI.Button(new Rect(Screen.width*4/6, Screen.width/80, Screen.width/6, Screen.height/10), "Aceptar")) {
 					player.addMission("Mision 1");
 					player.listMissions();
-					GUI.TextArea (new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7), "Mision 1 Aceptada");
+					GUI.TextArea (rectText, "Mision 1 Aceptada");
 				}else if (GUI.Button(new Rect(Screen.width*5/6, Screen.width/80, Screen.width/6, Screen.height/10), "Cancelar")){
-					GUI.TextArea (new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7), "Vuelve Pronto");
+					GUI.TextArea (rectText, "Vuelve Pronto");
 				}
 			}else{
-				GUI.TextArea (new Rect(Screen.width/6, Screen.width/100, Screen.width-Screen.width/6, Screen.height/7), "Hola, como estas, en que puedo ayudarte!");
+				GUI.TextArea (rectText, "Hola, como estas, en que puedo ayudarte!");
 			}
 			//texto.text = this.transform.parent.name + (this.transform.parent.name == "Jimmy");
 		} else if(this.transform.parent.name == "Nigga"){
 			if(player.state==1){
-				player.completeMission("Mision 1");
-				player.addMission("Mision 2");
-				GUI.TextArea (new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7), "Haga la mision 2");
-				player.listMissions();
+				GUI.TextArea (rectText, "Haga la mision 2");
+				if(GUI.Button(new Rect(Screen.width*4/6, Screen.width/80, Screen.width/6, Screen.height/10), "Aceptar")) {
+					player.addMission("Mision 2");
+					player.listMissions();
+					GUI.TextArea (rectText, "Mision 2 Aceptada");
+				}else if (GUI.Button(new Rect(Screen.width*5/6, Screen.width/80, Screen.width/6, Screen.height/10), "Cancelar")){
+					GUI.TextArea (rectText, "Vuelva Pronto, oyo?");
+				}
 			}else{
-				GUI.TextArea (new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7), "Que anda buscando el 'eñor");
+				GUI.TextArea (rectText, "Que necesita el 'eñor!!");
 			}
 			//texto.text = this.transform.parent.name + (this.transform.parent.name == "Capucho");
 		} else if(this.transform.parent.name == "ProfesorCuchilla"){
 			if(player.state==2){
-				player.completeMission("Mision 2");
-				player.addMission("Mision 3");
-				GUI.TextArea (new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7), "Haga la mision 3");
-				player.listMissions();
+				GUI.TextArea (rectText, "Haga la mision 3");
+				if(GUI.Button(new Rect(Screen.width*4/6, Screen.width/80, Screen.width/6, Screen.height/10), "Aceptar")) {
+					player.addMission("Mision 3");
+					player.listMissions();
+					GUI.TextArea (rectText, "Mision 3 Aceptada");
+				}else if (GUI.Button(new Rect(Screen.width*5/6, Screen.width/80, Screen.width/6, Screen.height/10), "Cancelar")){
+					GUI.TextArea (rectText, "Apurele!!");
+				}
 			}else{
-				GUI.TextArea (new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7), "Vaya a estudiar vago!!");
-			}
-			
+				GUI.TextArea (rectText, "Estudie Vago!!!");
+			}			
 			//texto.text = this.transform.parent.name + (this.transform.parent.name == "Capucho");
 		} else if(this.transform.parent.name == "Celador"){
 			if(player.state==3){
-				player.completeMission("Mision 3");
-				player.addMission("Mision 4");
-				GUI.TextArea (new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7), "Haga la mision 4");
-				player.listMissions();
+				GUI.TextArea (rectText, "Haga la mision 4");
+				if(GUI.Button(new Rect(Screen.width*4/6, Screen.width/80, Screen.width/6, Screen.height/10), "Aceptar")) {
+					player.addMission("Mision 4");
+					player.listMissions();
+					GUI.TextArea (rectText, "Mision 4 Aceptada");
+				}else if (GUI.Button(new Rect(Screen.width*5/6, Screen.width/80, Screen.width/6, Screen.height/10), "Cancelar")){
+					GUI.TextArea (rectText, "Lo espero dotor");
+				}
 			}else{
-				GUI.TextArea (new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7), "El carnet por favor!");
+				GUI.TextArea (rectText, "El carnet por favor");
 			}
 			//texto.text = this.transform.parent.name + (this.transform.parent.name == "Capucho");
 		} else if(this.transform.parent.name == "Capucho"){
 			if(player.state==4){
-				player.completeMission("Mision 4");
-				player.addMission("Mision 5");
-				GUI.TextArea (new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7), "Haga la mision 5");
-				player.listMissions();
+				GUI.TextArea (rectText, "Haga la mision 5");
+				if(GUI.Button(new Rect(Screen.width*4/6, Screen.width/80, Screen.width/6, Screen.height/10), "Aceptar")) {
+					player.addMission("Mision 5");
+					player.listMissions();
+					GUI.TextArea (rectText, "Mision 5 Aceptada");
+				}else if (GUI.Button(new Rect(Screen.width*5/6, Screen.width/80, Screen.width/6, Screen.height/10), "Cancelar")){
+					GUI.TextArea (rectText, "Lo espero compita");
+				}
 			}else{
-				GUI.TextArea (new Rect(Screen.width/6, Screen.width/100, Screen.width/6, Screen.height/7), "CAMILO VIVE!!!");
+				GUI.TextArea (rectText, "Camilo VIVE!!!");
 			}
 			//texto.text = this.transform.parent.name + (this.transform.parent.name == "Capucho");
 		}
