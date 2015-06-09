1f046ad67ecc3db0270fdc96c03982254ba16ea7
jdtorregrosas <jdtorregrosas@unal.edu.co>
2015-06-08 23:05:24 -0500

    minimapa implementado con misiones mostradas en el minimapa

--- UNReal_Fantasy/Assets/Project Assets/Scripts/signosMisiones.cs
+++ UNReal_Fantasy/Assets/Project Assets/Scripts/signosMisiones.cs
@@ -19,17 +19,25 @@ public class signosMisiones : MonoBehaviour {
 			personaje = GameObject.Find (misionActual.dueño);
 			exclamacion = personaje.GetComponentsInChildren<MeshRenderer> () [1];
 			exclamacion.enabled = true;
+			exclamacion = personaje.GetComponentsInChildren<MeshRenderer> () [3];
+			exclamacion.enabled = true;
 		} else if (misionActual.estado == Mision.ENPROGRESO) {
 			personaje = GameObject.Find (misionActual.dueño);
 			exclamacion = personaje.GetComponentsInChildren<MeshRenderer> () [1];
+			exclamacion.enabled = false;
+			exclamacion = personaje.GetComponentsInChildren<MeshRenderer> () [3];
 			exclamacion.enabled = false;
 		} else if (misionActual.estado == Mision.FINALIZADA) {
 			personaje = GameObject.Find (misionActual.finaliza);
 			interrogante = personaje.GetComponentsInChildren<MeshRenderer> () [0];
 			interrogante.enabled = true;
+			interrogante = personaje.GetComponentsInChildren<MeshRenderer> () [2];
+			interrogante.enabled = true;
 		} else if (misionActual.estado == Mision.ENTREGADA) {
 			personaje = GameObject.Find (misionActual.finaliza);
 			interrogante = personaje.GetComponentsInChildren<MeshRenderer> () [0];
+			interrogante.enabled = false;
+			interrogante = personaje.GetComponentsInChildren<MeshRenderer> () [2];
 			interrogante.enabled = false;
 		} 
 
