6b698b797de4ef88e1a4077c9ac7ac1169cd64fa
Fabian <flare900426@gmail.com>
2015-06-11 00:05:50 -0500

    -Commit de parte del cambio anterior.

--- UNReal_Fantasy/Assets/Project Assets/Scripts/Sistema de batallas/enemyStatusGUI.cs
+++ UNReal_Fantasy/Assets/Project Assets/Scripts/Sistema de batallas/enemyStatusGUI.cs
@@ -2,43 +2,43 @@
 using System.Collections;
 
 public class enemyStatusGUI : MonoBehaviour {
-
+	
 	
 	// texturas
 	public Texture2D statusLayoutBg; // fondo del layout para estatus
 	public Texture2D closeBtn;
-
+	
 	public Texture2D healthBackground; 	// fondo blanco (no el quee stan pensando)
-
+	
 	public Texture2D healthForeground; // barra roja
 	public Texture2D healthDamage; // barra amarilla de daño
 	
 	//public Texture2D staminaForeground; // barra roja
 	//public Texture2D staminaDamage; // barra amarilla de daño
-
+	
 	//public GUISkin HUDSkin; // skin alternativo (no esta en uso)
 	public GUIStyle style;
-
+	
 	//interfaz
 	public int layoutWidth=250;
 	public int layoutHeight=100;
 	public float maxHP=1000; 
 	public float curHP=0; 
-
+	
 	public string enemyName;
 	public GameObject gizmo_npc;
 	public bool showStatus=true;
-
+	
 	//Desde donde empiean los elementos 
 	private int anchorX = 0;			
 	private int anchorY = 0;
 	int barH = 20;
-
+	
 	//posiciones relativas al jugador
 	private GameObject player;
 	public int yGUIDistance=120;
 	public int selectableDistance=300;		
-
+	
 	//referencia de posicion a pasar al jugador atacante (para que el jugador sepa a donde dirigirse)
 	private Transform thisTransform;
 	
@@ -46,13 +46,13 @@ public class enemyStatusGUI : MonoBehaviour {
 	private float previousHealth; 
 	private float healthBar; 
 	private float myFloat; 
-
-
-
+	
+	
+	
 	//Variables para manejo de doble clic
 	private float lastClickTime=0f;
 	private float catchTime=0.4f;
-
+	
 	void Start () {
 		curHP=maxHP;
 		previousHealth = maxHP; 
@@ -60,19 +60,19 @@ public class enemyStatusGUI : MonoBehaviour {
 		anchorX = (int)(layoutWidth * 0.1f);
 		anchorY = (int)(layoutWidth * 0.05);
 		myFloat = (maxHP / 100) * 10; 
-
+		
 		//Ocultamos el gizmo (solo se muestra en mouseenter)
 		gizmo_npc.GetComponent<MeshRenderer>().enabled = false;
-
+		
 		if (!player)
 			player = GameObject.Find("PersonajePrincipal");
-
+		
 		thisTransform=GetComponent<Transform> ();
 	}
 	
 	void Update(){
 		adjustCurrentHealth();
-
+		
 		if(player.GetComponent<playerAttack>().getCurSkillIndex()!=-1&&
 		   Input.anyKey){
 			player.GetComponent<playerAttack> ().setAttackingEnemy (this);
@@ -80,7 +80,7 @@ public class enemyStatusGUI : MonoBehaviour {
 	}
 	
 	public void adjustCurrentHealth(){
-
+		
 		if(previousHealth > curHP){
 			previousHealth -= ((maxHP / curHP) * (myFloat)) * Time.deltaTime; // deducts health damage
 		} else {
@@ -103,25 +103,25 @@ public class enemyStatusGUI : MonoBehaviour {
 		if (showStatus) {								
 			float previousAdjustValue = (previousHealth * healthBar) / maxHP;
 			float percentage = healthBar * (curHP / maxHP);
-
+			
 			//GUI.skin = HUDSkin;
 			GUILayout.BeginArea (new Rect((Screen.width-layoutWidth)/2 , 10, layoutWidth, layoutHeight),"");		//Interfaz al centro de la pantalla
 			//Fondo del layout
 			GUI.DrawTexture (new Rect (0, 0,layoutWidth, layoutHeight), statusLayoutBg);  
-
+			
 			//Nombre
 			GUI.Label (new Rect (16,16,245,12),enemyName,style);
-
+			
 			//Icono de estado
 			//GUI.DrawTexture (new Rect (anchorX, anchorY, healthBar , barH), icon);  
-
+			
 			//Barra de salud
 			GUI.DrawTexture (new Rect (anchorX, 3*anchorY, healthBar, 20), healthBackground);       
 			GUI.DrawTexture (new Rect (anchorX, 3*anchorY, previousAdjustValue, 20), healthDamage);
 			GUI.DrawTexture (new Rect (anchorX, 3*anchorY, percentage, 20), healthForeground);
 			//Cantidad de salud
 			GUI.Label (new Rect (anchorX, 3*anchorY, healthBar, 20),(int)(previousHealth) + "/" + maxHP.ToString (),style);
-
+			
 			//Boton de cerrado
 			if(GUI.Button(new Rect(layoutWidth-anchorX, anchorY, 15, 15),closeBtn)){
 				hideStatus();
@@ -131,19 +131,19 @@ public class enemyStatusGUI : MonoBehaviour {
 			gizmo_npc.GetComponent<MeshRenderer>().enabled = true;
 		}
 	}
-
+	
 	void OnMouseDown(){
 		ShowStatus ();
 		if (Time.time - lastClickTime < catchTime) {
 			Debug.Log ("Enemigo clickeado");
 			player.GetComponent<playerAttack> ().setAttackingEnemy (this);
-
+			
 		} else {
 			player.GetComponent<playerAttack>().setIsTargetSelected(true);
 		}
 		lastClickTime=Time.time;
 	}
-
+	
 	//Muestra el estado, se autoselecciona para el jugador como enemigo activo y oculta las demas interfaces
 	public void ShowStatus(){
 		//deselecciona los demas enemigos marcados con el tag selectable (usado para seleccionables con interfaz emergente)
@@ -162,19 +162,19 @@ public class enemyStatusGUI : MonoBehaviour {
 		showStatus = false;
 		player.GetComponent<playerAttack> ().setAttackingEnemy (null);
 	}
-
-
+	
+	
 	void OnMouseEnter(){
 		gizmo_npc.GetComponent<MeshRenderer>().enabled = true;
 	}
 	void OnMouseOut(){
 		gizmo_npc.GetComponent<MeshRenderer>().enabled = false;
 	}
-
-
+	
+	
 	//setters y getters (evitan el exceso de variables publicas)
 	public Transform getTransform(){
 		return thisTransform;
 	}
-
+	
 }
