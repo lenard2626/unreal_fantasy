4b1c9b068c9490f0b81a990fc3bcad161e66f893
Cristian <ceparadac@unal.edu.co>
2015-06-11 01:56:58 -0500

    -script para pasos y golpe de personaje implementados. Comprobado funcionalidad correcta del script pasos

---
+++ UNReal_Fantasy/Assets/Project Assets/Scripts/pasos.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pasos : MonoBehaviour {
+		
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W))
+		{
+			GetComponent<AudioSource>().Play ();
+			
+		}
+		if (Input.GetKeyUp (KeyCode.W))
+		{
+			
+			GetComponent<AudioSource>().Stop ();
+			
+		}
+	}
+}
